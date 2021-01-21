2f0b2cfe35c0cd12bf3e66d2c36c8bd2fa754c98
BradleyMorrison1 <70822639+BradleyMorrison1@users.noreply.github.com>
2021-01-21 13:43:25 -0400

    Initial commit

---
+++ HealthSystem 3.0/Program.cs
@@ -0,0 +1,21 @@
+﻿// Bradley Morrison
+// 2021-01-21
+// NSCC - Game Development
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystem_3._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.ReadKey(true);
+        }
+    }
+}
